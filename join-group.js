f31e35174b60b93b35a29d91557b1bf0a9db1b3e
Ping Qu <pqu6@wisc.edu>
2021-04-19 13:24:07 -0500

    show (Admin) if the user is the admin of the group

--- join-group.js
+++ join-group.js
@@ -17,6 +17,7 @@ const queryURL = window.location.search;
 const urlParams = new URLSearchParams(queryURL);
 var groupId=urlParams.get("id");
 var pendingIdsSet = new Set();
+var groupAdminId;
 
 // collect title IDs into an iterable array
 var contentsArr = ["group-mem", "group-info"];
@@ -52,15 +53,12 @@ async function getGroupInfo(){
  
     let response = await fetch(url, fetchOptions);
     let data = await response.json();
-    console.log(data);
-    var info = "";
+    let info = "";
     Object.keys(data).forEach(function(key) {
-        info += key+" : " + data[key] + "<br>";
-        
-      
-      })
-    console.log(info);
+        info += key+" : " + data[key] + "<br>"
+      });
     document.getElementById("text-2").innerHTML = info;
+    groupAdminId = data["admin"];
 }
 
 getGroupInfo();
@@ -90,6 +88,7 @@ async function getMemberIds(){
     document.getElementById("text-1").innerHTML = result;
 }
 
+// user join group (add the user to the group member list)
 async function joinPublicGroup(){
     const url="http://127.0.0.1:6969/api/member/join/" + groupId;
     const fetchOptions = {
@@ -101,12 +100,14 @@ async function joinPublicGroup(){
     let data = await response.json();
     var error =  data["Error"];
     if(error != null){
-      alert(error);
+      alert(error);     // show message if the user already joined the group 
     }else{
-        var success = data["Success"];
-        alert(success);
+        var success = data["Success"]; 
+        alert(success);   // show message that need admin approval if the user join a private group
     }
 }
+
+
 
 async function getUserNameById(userId){
     const url="http://127.0.0.1:6969/id/" + userId;
@@ -116,9 +117,12 @@ async function getUserNameById(userId){
     };
     let response = await fetch(url, fetchOptions);
     let data = await response.json();
-    var userName = data["firstname"]+" "+data["lastname"];
+    var userName = data["firstname"]+" "+data["lastname"]; 
     if(pendingIdsSet.has(userId)){
-        userName += " (Pending)";
+        userName += " (Pending)";    // show (Pending) status if the users need admin approval
+    }
+    if(userId==groupAdminId){
+        userName += " (Admin)";  // show (Admin) if the user is admin of the group
     }
     return userName;
 }
