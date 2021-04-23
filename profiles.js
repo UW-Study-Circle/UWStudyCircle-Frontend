c3534d267378a962a7fde8cd6de6a9e57e5583f7
Ping Qu <pqu6@wisc.edu>
2021-04-23 14:18:10 -0500

    Do not turn to group page if the user join a Private group

--- profiles.js
+++ profiles.js
@@ -111,7 +111,7 @@ async function joinPrivateGroup(groupId){
   var error =  data["Error"];
   var msg = "This is a private group.\n\n";
   if(error != null){
-    alert(msg+ error);     // show message if the user already joined the group 
+    alert(msg+ "Waiting for Admin Approval.");     // show message if the user already joined the group 
   }else{
       var success = data["Success"]; 
       alert(msg+success);   // show message that need admin approval if the user join a private group
